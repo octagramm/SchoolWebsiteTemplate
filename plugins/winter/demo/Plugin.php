f014b8d7f2248a3384bcc08c3991e04adb676286
Stepan Farlom <stepan.farlom@gmail.com>
2024-09-11 16:15:10 +0300

    header dropdown pages

--- plugins/winter/demo/Plugin.php
+++ plugins/winter/demo/Plugin.php
@@ -4,6 +4,7 @@
  * The plugin.php file (called the plugin initialization script) defines the plugin information class.
  */
 
+use Illuminate\Support\Facades\View;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
@@ -24,5 +25,15 @@ class Plugin extends PluginBase
         return [
             '\Winter\Demo\Components\Todo' => 'demoTodo'
         ];
+    }
+
+    public function register()
+    {
+        View::share('name', 'Школа №123');
+        View::share('fullName', 'МОУ "Средняя школа №123"');
+        View::share('orgName', 'Школа');
+        View::share('orgNumber', '№123');
+        View::share('phoneNumber', '+7(999)-999-9999');
+        View::share('emailAddress', 'info@example.com');
     }
 }
